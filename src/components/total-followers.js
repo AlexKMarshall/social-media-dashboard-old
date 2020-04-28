f1c3c43389004a2122f2cc9c4205ae7bf1e6bb02
Alex Marshall <alex.k.marshall83@gmail.com>
2020-04-28 09:43:58 +0100

    Add state to TotalFollowers

--- src/components/total-followers.js
+++ src/components/total-followers.js
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
-const TotalFollowers = () => <div>Total Followers: *follower count*</div>;
+const TotalFollowers = () => {
+  const [followerCount] = useState(0);
+
+  const formatCount = (count) => count.toLocaleString(undefined);
+
+  return <div>Total Followers: {formatCount(followerCount)}</div>;
+};
 
 export default TotalFollowers;
