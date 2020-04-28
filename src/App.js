cb373e7feabacbbd5e3952319e3bb2563981f162
Alex Marshall <alex.k.marshall83@gmail.com>
2020-04-28 09:32:04 +0100

    Add follower count component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import TotalFollowers from "./components/total-followers";
 
 function App() {
   return (
     <div className="App">
       <h1>Social Media Dashboard</h1>
+      <TotalFollowers />
     </div>
   );
 }
---
+++ src/components/total-followers.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const TotalFollowers = () => <div>Total Followers: *follower count*</div>;
+
+export default TotalFollowers;
