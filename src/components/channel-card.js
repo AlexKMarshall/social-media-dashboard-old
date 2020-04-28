620a0091f19d01c972a00d91bee9649a94ffb91e
Alex Marshall <alex.k.marshall83@gmail.com>
2020-04-28 11:57:08 +0100

    Map out basic card component markup

--- src/components/channel-card.js
+++ src/components/channel-card.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const ChannelCard = ({ id }) => {
-  return <div>I'm a card for network number {id}</div>;
+  return (
+    <section>
+      <header>
+        <div>Channel Logo</div>
+        <div>username</div>
+      </header>
+      <div>1234</div>
+      <div>followers</div>
+      I'm a card for network number {id}
+      <footer>
+        <div>direction icon</div>
+        <div>follower delta</div>
+        Today
+      </footer>
+    </section>
+  );
 };
 
 export default ChannelCard;
