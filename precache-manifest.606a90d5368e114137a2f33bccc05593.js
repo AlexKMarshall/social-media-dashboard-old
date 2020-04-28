a18e2ee40d7e78cda78fdd32f67b78d224b360f2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-28 09:40:38 +0000

    Deploy AlexKMarshall/social-media-dashboard to github.com/AlexKMarshall/social-media-dashboard.git:gh-pages

---
+++ precache-manifest.606a90d5368e114137a2f33bccc05593.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c5ec0245a3177fe48fd0ea1bdd230a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3e37828b65a4fa82421",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1cf7ff800fadb2151248",
+    "url": "/static/js/2.de161c3c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.de161c3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3e37828b65a4fa82421",
+    "url": "/static/js/main.3c22709d.chunk.js"
+  },
+  {
+    "revision": "c270996d6a2071b13e9d",
+    "url": "/static/js/runtime-main.deafe215.js"
+  }
+]);
--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const heading = getByText(/social/i);
-  expect(heading).toBeInTheDocument();
-});
