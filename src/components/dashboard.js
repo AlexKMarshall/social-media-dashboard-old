bb3fc803e06525168ae795eb57fdce364447dafb
Alex Marshall <alex.k.marshall83@gmail.com>
2020-05-01 17:08:53 +0100

    Add margin to dashboard

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,10 +2,11 @@ import React from "react";
 import TotalFollowers from "./total-followers";
 import DarkModeSwitch from "./dark-mode-switch";
 import SocialFollowersList from "./social-followers-list";
+import "./dashboard.css";
 
 const Dashboard = () => {
   return (
-    <div className="App">
+    <div className="dashboard">
       <header>
         <h1>Social Media Dashboard</h1>
         <TotalFollowers />
