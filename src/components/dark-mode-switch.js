67b4a8b2a1ba3979adb46930f1f5d3514628540e
Alex Marshall <alex.k.marshall83@gmail.com>
2020-05-02 19:31:44 +0100

    Toggle between light and dark themes

--- src/components/dark-mode-switch.js
+++ src/components/dark-mode-switch.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-const DarkModeSwitch = () => {
+const DarkModeSwitch = ({ toggle }) => {
   return (
     <div>
       <label for="dark-mode-toggle">Dark Mode</label>
-      <input type="checkbox" name="dark-mode-toggle" id="dark-mode-toggle" />
+      <input
+        type="checkbox"
+        name="dark-mode-toggle"
+        id="dark-mode-toggle"
+        onClick={toggle}
+      />
     </div>
   );
 };
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,24 +1,28 @@
 import React, { useState } from "react";
 import TotalFollowers from "./total-followers";
-// import DarkModeSwitch from "./dark-mode-switch";
+import DarkModeSwitch from "./dark-mode-switch";
 import HeadlineList from "./headline-list";
 import { headlineDataStore } from "./../assets/data/data-store";
 import OverviewList from "./overview-list";
 
 const Dashboard = () => {
   const [headlineData] = useState(headlineDataStore);
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () =>
+    setTheme((theme) => (theme === "light" ? "dark" : "light"));
 
   const totalFollowers = headlineData
     .map((channel) => Number.parseInt(channel.followerCount))
     .reduce((acc, cur) => acc + cur);
 
   return (
-    <div className="dashboard dark-theme">
+    <div className={`dashboard ${theme}-theme`}>
       <header>
         <h1 className="impact-text">Social Media Dashboard</h1>
         <TotalFollowers count={totalFollowers} />
       </header>
-      {/* <DarkModeSwitch /> */}
+      <DarkModeSwitch toggle={toggleTheme} />
       <section id="headlines">
         <HeadlineList followerData={headlineData} />
       </section>
