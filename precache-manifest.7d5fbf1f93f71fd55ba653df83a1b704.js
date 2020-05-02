ee277615f12cdb8f009e61cad7fb521655ff4c65
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-02 11:24:27 +0000

    Deploy AlexKMarshall/social-media-dashboard to github.com/AlexKMarshall/social-media-dashboard.git:gh-pages

---
+++ precache-manifest.7d5fbf1f93f71fd55ba653df83a1b704.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "188ec3f09f1102dff77f49df2fe64816",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19c0cdc784f77f493367",
+    "url": "/static/css/main.588bd236.chunk.css"
+  },
+  {
+    "revision": "52b0df99b823c613a70c",
+    "url": "/static/js/2.f409f712.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f409f712.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19c0cdc784f77f493367",
+    "url": "/static/js/main.003708fb.chunk.js"
+  },
+  {
+    "revision": "c270996d6a2071b13e9d",
+    "url": "/static/js/runtime-main.deafe215.js"
+  },
+  {
+    "revision": "69a5c77f51678c145d64af371ff73b22",
+    "url": "/static/media/icon-down.69a5c77f.svg"
+  },
+  {
+    "revision": "a738376499ac8a902fd2ce03c3030f98",
+    "url": "/static/media/icon-facebook.a7383764.svg"
+  },
+  {
+    "revision": "991487fe03dace6f69a870d3085aadf1",
+    "url": "/static/media/icon-instagram.991487fe.svg"
+  },
+  {
+    "revision": "810ccee84b0132ca7921a1b0b4ee6021",
+    "url": "/static/media/icon-twitter.810ccee8.svg"
+  },
+  {
+    "revision": "95f916a549511a0912328d5626522d2c",
+    "url": "/static/media/icon-up.95f916a5.svg"
+  },
+  {
+    "revision": "8499da129aedb70f3e20262951f7e236",
+    "url": "/static/media/icon-youtube.8499da12.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.606a90d5368e114137a2f33bccc05593.js"
+  "/precache-manifest.7d5fbf1f93f71fd55ba653df83a1b704.js"
 );
 
 self.addEventListener('message', (event) => {
