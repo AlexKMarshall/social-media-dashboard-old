8dcccde949c9f5fa1d9dfb2e790f12eb29a9c83e
Alex Marshall <alex.k.marshall83@gmail.com>
2020-05-02 19:42:24 +0100

    Correct label for attribute to htmlFor

--- src/components/dark-mode-switch.js
+++ src/components/dark-mode-switch.js
@@ -3,7 +3,7 @@ import React from "react";
 const DarkModeSwitch = ({ toggle }) => {
   return (
     <div>
-      <label for="dark-mode-toggle">Dark Mode</label>
+      <label htmlFor="dark-mode-toggle">Dark Mode</label>
       <input
         type="checkbox"
         name="dark-mode-toggle"
