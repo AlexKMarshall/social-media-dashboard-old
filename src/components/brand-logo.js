f1d519c643c8f1592335095181112c941f40502b
Alex Marshall <alex.k.marshall83@gmail.com>
2020-05-02 11:43:13 +0100

    Add twitter and insta logos

--- src/components/brand-logo.js
+++ src/components/brand-logo.js
@@ -1,10 +1,14 @@
 import React from "react";
 import iconFacebook from "../assets/icons/icon-facebook.svg";
 import iconYoutube from "../assets/icons/icon-youtube.svg";
+import iconTwitter from "../assets/icons/icon-twitter.svg";
+import iconInstagram from "../assets/icons/icon-instagram.svg";
 
 const brandIconMap = {
   facebook: iconFacebook,
   youtube: iconYoutube,
+  twitter: iconTwitter,
+  instagram: iconInstagram,
 };
 
 const BrandLogo = ({ brand }) => {
